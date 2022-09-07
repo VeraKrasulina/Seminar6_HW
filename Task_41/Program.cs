1f6485b8c2a32b48f60c499f7291498c4142fb58
Vera Krasulina <kshisinka@hotmail.com>
2022-09-07 15:06:43 +0300

    Task 41

---
+++ Task_41/Program.cs
@@ -0,0 +1,28 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+Console.Write("Введите числа через пробел: ");
+string imput = Console.ReadLine();
+
+int[] ConvertToIntArrey(string text, char separator)
+{
+    string[] imput = text.Split(separator);
+    int[] numbers = new int[imput.Length];
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        int j;
+        string temp = imput[i];
+        if (int.TryParse(temp, out j)) { numbers[i] = j; }
+    }
+    return numbers;
+}
+
+int[] output = ConvertToIntArrey(imput, ' ');
+
+int counter = 0;
+for (int i = 0; i < output.Length; i++)
+{
+    counter += output[i] > 0 ? 1 : 0;
+}
+
+Console.WriteLine($"Количество чисел больше 0 в этом списке - {counter}.");
